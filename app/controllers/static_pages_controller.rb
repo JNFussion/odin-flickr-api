7b41f936a7c34dee202675b08bd649cad347a9c4
JNFussion <juan.jose.gutierrez.99@gmail.com>
2021-03-31 12:17:16 +0200

    URL Photos are now from Flickr helper. Add begin-recue to controller in case the user is not found

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,21 +6,15 @@ class StaticPagesController < ApplicationController
     @flickr = Flickr.new
     
     if params[:user_id]
-      @user = @flickr.people.getInfo :user_id => params[:user_id]
-      @photos = @flickr.people.getPublicPhotos :user_id => params[:user_id]
-      get_photos_url
+      begin
+        @user = @flickr.people.getInfo :user_id => params[:user_id]
+        @photos = @flickr.people.getPublicPhotos :user_id => params[:user_id]
+      rescue Flickr::FailedResponse
+        flash[:alert] = "User not found"
+        render :index
+      end
+      
     end
-  end
-
-  private
-
-  def get_photos_url
-    @photos_url = Array.new
-
-    @photos.each do |photo|
-      @photos_url << "https://live.staticflickr.com/#{photo.server}/#{photo.id}_#{photo.secret}.jpg"
-    end
-
   end
 
 end
